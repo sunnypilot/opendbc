8a16207748d02874b012b8acc1876269ea4bc698
Jason Wen <haibin.wen3@gmail.com>
2024-11-19 14:15:13 -0500

    final

--- opendbc/sunnypilot/car/hyundai/values.py
+++ opendbc/sunnypilot/car/hyundai/values.py
@@ -3,8 +3,6 @@ from enum import IntFlag
 
 class HyundaiFlagsSP(IntFlag):
   """
-  Represents special flags for Hyundai specific features within sunnypilot.
-  These flags will go into the `sunnypilotFlags` field of the `CarParams` (car.capnp)
-  Max 32 flags can be defined in this class.
+    Flags for Hyundai specific quirks within sunnypilot.
   """
   ENHANCED_SCC = 1
