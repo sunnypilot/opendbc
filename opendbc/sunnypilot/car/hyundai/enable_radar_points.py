df099f7bea8252c68d49784af92b61025440fe48
Jason Wen <haibin.wen3@gmail.com>
2025-01-10 15:55:53 -0500

    better

--- opendbc/sunnypilot/car/hyundai/enable_radar_points.py
+++ opendbc/sunnypilot/car/hyundai/enable_radar_points.py
@@ -6,10 +6,11 @@ DEVELOPER_DIAGNOSTIC = 0x07
 CUSTOM_DIAGNOSTIC_REQUEST = bytes([uds.SERVICE_TYPE.DIAGNOSTIC_SESSION_CONTROL, DEVELOPER_DIAGNOSTIC])
 CUSTOM_DIAGNOSTIC_RESPONSE = bytes([uds.SERVICE_TYPE.DIAGNOSTIC_SESSION_CONTROL + 0x40, DEVELOPER_DIAGNOSTIC])
 
+WRITE_DATA_REQUEST = bytes([uds.SERVICE_TYPE.WRITE_DATA_BY_IDENTIFIER])
 WRITE_DATA_RESPONSE = bytes([uds.SERVICE_TYPE.WRITE_DATA_BY_IDENTIFIER + 0x40])
 
-CONFIG_DATA_ID = [0x01, 0x42]
-RADAR_TRACKS_CONFIG = [0x00, 0x00, 0x00, 0x01, 0x00, 0x01]
+CONFIG_DATA_ID = bytes([0x01, 0x42])
+RADAR_TRACKS_CONFIG = bytes([0x00, 0x00, 0x00, 0x01, 0x00, 0x01])
 
 
 def enable_radar_tracks(logcan, sendcan, bus=0, addr=0x7d0, timeout=0.1, retry=10, debug=False):
@@ -22,7 +23,7 @@ def enable_radar_tracks(logcan, sendcan, bus=0, addr=0x7d0, timeout=0.1, retry=1
       for _, _ in query.get_data(timeout).items():
         carlog.warning("radar_tracks: reconfigure radar to output radar points ...")
 
-        request = bytes([uds.SERVICE_TYPE.WRITE_DATA_BY_IDENTIFIER, CONFIG_DATA_ID, RADAR_TRACKS_CONFIG])
+        request = WRITE_DATA_REQUEST + CONFIG_DATA_ID + RADAR_TRACKS_CONFIG
         query = IsoTpParallelQuery(sendcan, logcan, bus, [addr], [request], [WRITE_DATA_RESPONSE], debug=debug)
         query.get_data(0)
 
