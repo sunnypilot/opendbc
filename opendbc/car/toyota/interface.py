fd5f3f415191cc9816f07a7bed590fd806f44d1d
rav4kumar <meetkumardesai@gmail.com>
2025-03-13 07:03:28 -0700

    smoother stopping?

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -153,7 +153,7 @@ class CarInterface(CarInterfaceBase):
       else:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
-        ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+        ret.stoppingDecelRate = 0.006  # reach stopping target smoothly
 
       # Hybrids have much quicker longitudinal actuator response
       if ret.flags & ToyotaFlags.HYBRID.value:
