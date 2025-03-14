5fd3d88170ba9fee14c326932ff3267632d391c7
rav4kumar <meetkumardesai@gmail.com>
2025-03-14 07:50:46 -0700

    smoother stopping?

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -149,7 +149,7 @@ class CarInterface(CarInterfaceBase):
       if sp_tss2_long_tune:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.22
-        ret.stoppingDecelRate = 0.0036  # reach stopping target smoothly
+        ret.stoppingDecelRate = 0.006  # reach stopping target smoothly
       else:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
