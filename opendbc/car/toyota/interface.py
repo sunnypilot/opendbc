7c635e34f35b6315fb47080506c6e0a652084ffd
rav4kumar <meetkumardesai@gmail.com>
2025-03-23 07:36:24 -0700

    ss

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -171,7 +171,7 @@ class CarInterface(CarInterfaceBase):
       if sp_tss2_long_tune:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.22
-        ret.stoppingDecelRate = 0.007  # reach stopping target smoothly
+        ret.stoppingDecelRate = 0.004  # reach stopping target smoothly
       else:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
