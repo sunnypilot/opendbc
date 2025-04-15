fb4ceadcbcf6fe80009cd7a2a644400e2a19afe6
chrispypatt <christriplenickel@yahoo.com>
2025-04-14 20:09:51 -0500

    Update tests

--- opendbc/sunnypilot/car/toyota/tests/test_secoc_long_base.py
+++ opendbc/sunnypilot/car/toyota/tests/test_secoc_long_base.py
@@ -13,6 +13,7 @@ def car_params():
   params = structs.CarParams()
   params.carFingerprint = "TOYOTA_RAV4_PRIME"
   params.flags = ToyotaFlags.SECOC.value
+  params.openpilotLongitudinalControl = True
   return params
 
 
@@ -52,12 +53,27 @@ class TestSecOCLong:
     secoc_long.update_car_state(car_state)
     assert secoc_long.car_state == car_state
 
-  def test_update_accel_command(self, car_state):
+  @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+  @given(st.integers(min_value=0, max_value=255))
+  def test_update_accel_command(self, car_state, value):
     secoc_long = SecOCLong(car_state.CP)
     secoc_long.set_can_sends([])
     secoc_long.update_car_state(car_state)
     accel_message = {
-      "ACCEL_CMD": 1
+      "ACCEL_CMD": value
     }
     secoc_long.update_accel_command(self.packer, accel_message)
     assert accel_message["ACCEL_CMD"] == 0
+
+  @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+  @given(st.integers(min_value=0, max_value=255))
+  def test_sould_not_update_accel_command(self, car_state, value):
+    car_state.CP.openpilotLongitudinalControl = False
+    secoc_long = SecOCLong(car_state.CP)
+    secoc_long.set_can_sends([])
+    secoc_long.update_car_state(car_state)
+    accel_message = {
+      "ACCEL_CMD": value
+    }
+    secoc_long.update_accel_command(self.packer, accel_message)
+    assert accel_message["ACCEL_CMD"] == value
