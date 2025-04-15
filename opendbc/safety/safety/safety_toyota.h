e7303d798e8aa3457f3e052a461ace0552cb04b8
Jason Wen <haibin.wen3@gmail.com>
2025-04-14 23:14:29 -0400

    combine desired_accel get function

--- opendbc/safety/safety/safety_toyota.h
+++ opendbc/safety/safety/safety_toyota.h
@@ -85,6 +85,13 @@ static bool toyota_get_quality_flag_valid(const CANPacket_t *to_push) {
   return valid;
 }
 
+static int toyota_get_longitudinal_desired_accel_tx(const CANPacket_t *to_send) {
+  int desired_accel = (GET_BYTE(to_send, 0) << 8) | GET_BYTE(to_send, 1);
+  desired_accel = to_signed(desired_accel, 16);
+
+  return desired_accel;
+}
+
 static void toyota_rx_hook(const CANPacket_t *to_push) {
   if (GET_BUS(to_push) == 0U) {
     int addr = GET_ADDR(to_push);
@@ -220,8 +227,7 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   if (bus == 0) {
     // ACCEL: safety check on byte 1-2
     if (addr == 0x343) {
-      int desired_accel = (GET_BYTE(to_send, 0) << 8) | GET_BYTE(to_send, 1);
-      desired_accel = to_signed(desired_accel, 16);
+      int desired_accel = toyota_get_longitudinal_desired_accel_tx(to_send);
 
       bool violation = false;
       violation |= longitudinal_accel_checks(desired_accel, TOYOTA_LONG_LIMITS);
@@ -248,8 +254,7 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
 
     // ACCEL: safety check on byte 1-2 for SecOC car
     if (addr == 0x183) {
-      int desired_accel = (GET_BYTE(to_send, 0) << 8) | GET_BYTE(to_send, 1);
-      desired_accel = to_signed(desired_accel, 16);
+      int desired_accel = toyota_get_longitudinal_desired_accel_tx(to_send);
 
       bool violation = false;
       violation |= longitudinal_accel_checks(desired_accel, TOYOTA_LONG_LIMITS);
