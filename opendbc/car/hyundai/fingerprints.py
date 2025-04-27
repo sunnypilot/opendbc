d165ba186cb62da4874df213238bd9242dd1abc1
Jason Wen <haibin.wen3@gmail.com>
2025-04-26 23:38:09 -0400

    use new helpers

--- opendbc/car/hyundai/fingerprints.py
+++ opendbc/car/hyundai/fingerprints.py
@@ -1,5 +1,6 @@
 from opendbc.car.structs import CarParams
 from opendbc.car.hyundai.values import CAR
+from opendbc.sunnypilot.car.fw_versions_ext import merge_fw_versions
 
 Ecu = CarParams.Ecu
 
@@ -1240,3 +1241,5 @@ FW_VERSIONS = {
     ],
   },
 }
+
+FW_VERSIONS = merge_fw_versions(FW_VERSIONS)
