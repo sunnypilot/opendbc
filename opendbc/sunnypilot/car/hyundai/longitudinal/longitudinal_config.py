a4bba4161486ff1da53a33e6d889db63226a15ff
Discountchubbs <159560811+Discountchubbs@users.noreply.github.com>
2025-04-28 13:40:06 -0700

    Update longitudinal_config.py

--- opendbc/sunnypilot/car/hyundai/longitudinal/longitudinal_config.py
+++ opendbc/sunnypilot/car/hyundai/longitudinal/longitudinal_config.py
@@ -19,21 +19,21 @@ class CarTuningConfig:
   lookahead_jerk_upper_v: list[float] = field(default_factory=lambda: [0.25, 0.5])
   lookahead_jerk_lower_v: list[float] = field(default_factory=lambda: [0.15, 0.3])
   longitudinal_actuator_delay: float = 0.45
-  jerk_limits: float = 3.3
+  jerk_limits: float = 4.0
 
 
 # Default configurations for different car types
 # Min jerk is set to 0.53 per (Horn et al., 2024)
 TUNING_CONFIGS = {
   "CANFD": CarTuningConfig(
-    stopping_decel_rate=0.275,
+    stopping_decel_rate=0.4,
   ),
   "EV": CarTuningConfig(
     longitudinal_actuator_delay=0.45,
   ),
   "HYBRID": CarTuningConfig(
     v_ego_starting=0.12,
-    stopping_decel_rate=0.30,
+    stopping_decel_rate=0.4,
   ),
   "DEFAULT": CarTuningConfig(
     lookahead_jerk_bp=[2., 5., 20.],
@@ -47,8 +47,8 @@ CAR_SPECIFIC_CONFIGS = {
   CAR.KIA_NIRO_EV: CarTuningConfig(
     v_ego_stopping=0.1,
     stopping_decel_rate=0.1,
-    lookahead_jerk_upper_v=[0.6, 1.0],
-    lookahead_jerk_lower_v=[0.6, 0.65],
+    lookahead_jerk_upper_v=[0.7, 1.0],
+    lookahead_jerk_lower_v=[0.7, 0.85],
     jerk_limits=2.5,
     longitudinal_actuator_delay=0.45,
   ),
