d3de6b02dc92ab91d3e42c2e902167f3e20fe4a5
Jason Wen <haibin.wen3@gmail.com>
2025-05-10 23:28:32 -0400

    c prototype

--- opendbc/safety/safety/safety_subaru_common.h
+++ opendbc/safety/safety/safety_subaru_common.h
@@ -1,11 +1,16 @@
-#pragma once
+/**
+ * Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
+ *
+ * This file is part of sunnypilot and is licensed under the MIT License.
+ * See the LICENSE.md file in the root directory for more details.
+ */
 
-#include "safety_declarations.h"
+#pragma once
 
 extern bool subaru_stop_and_go;
 bool subaru_stop_and_go = false;
 
-void subaru_common_init() {
+void subaru_common_init(void) {
   const int SUBARU_PARAM_SP_STOP_AND_GO = 1;
 
   subaru_stop_and_go = GET_FLAG(current_safety_param_sp, SUBARU_PARAM_SP_STOP_AND_GO);
