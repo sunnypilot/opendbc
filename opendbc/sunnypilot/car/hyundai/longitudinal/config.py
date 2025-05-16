64f1388ed46f2c7f7fd7f7ce826e46d2ed096afa
discountchubbs <alexgrant990@gmail.com>
2025-05-16 06:29:31 -0700

    dec

--- opendbc/sunnypilot/car/hyundai/longitudinal/config.py
+++ opendbc/sunnypilot/car/hyundai/longitudinal/config.py
@@ -44,9 +44,11 @@ TUNING_CONFIGS = {
 # Car-specific configs
 CAR_SPECIFIC_CONFIGS = {
   CAR.KIA_NIRO_EV: CarTuningConfig(
-    stopping_decel_rate=0.3,
-    lookahead_jerk_upper_v=[0.3, 1.0],
-    lookahead_jerk_lower_v=[0.2, 0.3],
+    stopping_decel_rate=0.35,
+    v_ego_stopping=0.1,
+    lookahead_jerk_bp=[2., 5., 20.],
+    lookahead_jerk_upper_v=[0.5, 0.5, 1.0],
+    lookahead_jerk_lower_v=[0.05, 0.10, 0.5],
     jerk_limits=2.5,
   ),
   CAR.HYUNDAI_IONIQ: CarTuningConfig(
