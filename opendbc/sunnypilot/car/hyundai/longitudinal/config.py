50b3f7d27823a6cf8000771e2a297d735c16cdf0
discountchubbs <alexgrant990@gmail.com>
2025-05-21 09:41:32 -0700

    .05 for me

--- opendbc/sunnypilot/car/hyundai/longitudinal/config.py
+++ opendbc/sunnypilot/car/hyundai/longitudinal/config.py
@@ -45,7 +45,7 @@ TUNING_CONFIGS = {
 CAR_SPECIFIC_CONFIGS = {
   CAR.KIA_NIRO_EV: CarTuningConfig(
     stopping_decel_rate=0.35,
-    v_ego_stopping=0.1,
+    v_ego_stopping=0.05,
     lookahead_jerk_bp=[2., 5., 20.],
     lookahead_jerk_upper_v=[0.5, 0.5, 1.0],
     lookahead_jerk_lower_v=[0.05, 0.10, 0.5],
