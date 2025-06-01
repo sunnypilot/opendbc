fd35b5cc4bd8f85d5964e43e6319f31b9914623e
royjr <royjr96@gmail.com>
2025-06-01 00:34:57 -0400

    only curve on LANE_CHANGE_SPEED_MIN

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -154,15 +154,12 @@ def create_ccnc(packer, CAN, openpilotLongitudinalControl, enabled, hud, leftBli
     msg_161["SOUNDS_4"] = 0
 
   LANE_CHANGE_SPEED_MIN = 8.9408  # 20 * 0.44704
-  curvature = int(out.steeringAngleDeg / 3)
 
   msg_161.update({
     "DAW_ICON": 0,
     "LKA_ICON": 0,
     "LFA_ICON": 2 if lfa_icon else 0,
     "CENTERLINE": 1 if lfa_icon else 0,
-    "LANELINE_CURVATURE": (min(abs(curvature), 15) + (-1 if curvature < 0 else 0)) if lfa_icon else 0,
-    "LANELINE_CURVATURE_DIRECTION": 1 if curvature < 0 and lfa_icon else 0,
     "LANELINE_LEFT": (0 if not lfa_icon else 1 if not hud.leftLaneVisible else 4 if hud.leftLaneDepart else 6 if leftBlinker or rightBlinker else 2),
     "LANELINE_RIGHT": (0 if not lfa_icon else 1 if not hud.rightLaneVisible else 4 if hud.rightLaneDepart else 6 if leftBlinker or rightBlinker else 2),
     "LCA_LEFT_ICON": (0 if not lfa_icon or out.vEgo < LANE_CHANGE_SPEED_MIN else 1 if out.leftBlindspot else 2 if leftBlinker or rightBlinker else 4),
@@ -170,6 +167,11 @@ def create_ccnc(packer, CAN, openpilotLongitudinalControl, enabled, hud, leftBli
     "LCA_LEFT_ARROW": 2 if leftBlinker else 0,
     "LCA_RIGHT_ARROW": 2 if rightBlinker else 0,
   })
+
+  if lfa_icon and out.vEgo >= LANE_CHANGE_SPEED_MIN:
+    curvature = int(out.steeringAngleDeg / 2)
+    msg_161["LANELINE_CURVATURE"] = (min(abs(curvature), 15) + (-1 if curvature < 0 else 0))
+    msg_161["LANELINE_CURVATURE_DIRECTION"] = 1 if curvature < 0 else 0
 
   if lfa_icon and (leftBlinker or rightBlinker):
     leftlanequal = msg_1b5["LEFT_QUAL"]
