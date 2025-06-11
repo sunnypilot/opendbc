871e292cfe47ed6999d5b36cd184887cf46f3256
nayan8teen <nayan8teen@gmail.com>
2025-06-11 13:45:18 -0400

    reduce torque a bit

--- opendbc/safety/modes/hyundai_canfd.h
+++ opendbc/safety/modes/hyundai_canfd.h
@@ -144,10 +144,10 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *to_push) {
 
 static bool hyundai_canfd_tx_hook(const CANPacket_t *to_send) {
   const TorqueSteeringLimits HYUNDAI_CANFD_STEERING_LIMITS = {
-    .max_torque = 350,
+    .max_torque = 335,
     .max_rt_delta = 112,
-    .max_rate_up = 5,
-    .max_rate_down = 5,
+    .max_rate_up = 3,
+    .max_rate_down = 3,
     .driver_torque_allowance = 250,
     .driver_torque_multiplier = 2,
     .type = TorqueDriverLimited,
