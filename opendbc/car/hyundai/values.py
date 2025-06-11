35a0904f886ada873dbab047ce7371eea47167df
nayan8teen <nayan8teen@gmail.com>
2025-06-11 19:00:28 -0400

    reset torque

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -25,12 +25,12 @@ class CarControllerParams:
     self.STEER_STEP = 1  # 100 Hz
 
     if CP.flags & HyundaiFlags.CANFD:
-      self.STEER_MAX = 335
+      self.STEER_MAX = 33
       self.STEER_DRIVER_ALLOWANCE = 250
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 250
-      self.STEER_DELTA_UP = 3
-      self.STEER_DELTA_DOWN = 4
+      self.STEER_DELTA_UP = 2
+      self.STEER_DELTA_DOWN = 3
 
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
     # If the max stock LKAS request is <384, add your car to this list.
