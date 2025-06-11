a9b30555e52dc530c17692777cd0fa62440c0b3f
nayan8teen <nayan8teen@gmail.com>
2025-06-11 15:06:00 -0400

    adjust more

--- opendbc/safety/modes/hyundai_canfd.h
+++ opendbc/safety/modes/hyundai_canfd.h
@@ -147,7 +147,7 @@ static bool hyundai_canfd_tx_hook(const CANPacket_t *to_send) {
     .max_torque = 335,
     .max_rt_delta = 112,
     .max_rate_up = 3,
-    .max_rate_down = 3,
+    .max_rate_down = 4,
     .driver_torque_allowance = 250,
     .driver_torque_multiplier = 2,
     .type = TorqueDriverLimited,
