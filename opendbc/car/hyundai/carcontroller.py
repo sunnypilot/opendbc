6e9a10e0fcdd2b66fbd0cb18320e1a7fd4f121a4
DevTekVE <devtekve@gmail.com>
2025-06-17 12:13:51 +0200

    Redo 5 max angle rate

--- opendbc/car/hyundai/carcontroller.py
+++ opendbc/car/hyundai/carcontroller.py
@@ -34,7 +34,7 @@ MAX_FAULT_ANGLE = 85
 MAX_FAULT_ANGLE_FRAMES = 89
 MAX_FAULT_ANGLE_CONSECUTIVE_FRAMES = 2
 
-MAX_ANGLE_RATE = 2 # Was 5, but I am not trusting it!
+MAX_ANGLE_RATE = 5
 # Add extra tolerance for average banked road since safety doesn't have the roll
 AVERAGE_ROAD_ROLL = 0.06  # ~3.4 degrees, 6% superelevation. higher actual roll lowers lateral acceleration
 MAX_LATERAL_ACCEL = ISO_LATERAL_ACCEL + (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_ROAD_ROLL)  # ~3.6 m/s^2
