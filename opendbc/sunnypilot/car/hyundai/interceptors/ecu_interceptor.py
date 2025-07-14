eb13f53c2ee167c98771a03a256385c3dfc4fb03
DevTekVE <devtekve@gmail.com>
2025-07-14 23:20:24 +0200

    feat(hyundai): add base classes for ECU interceptors
    - Added abstract base classes for ECU interceptors, car state, car controller, and radar interface.
    - Introduced `AdasDrvEcuInterceptor` implementation for specific Hyundai ADAS ECU needs.

---
+++ opendbc/sunnypilot/car/hyundai/interceptors/ecu_interceptor.py
@@ -0,0 +1,65 @@
+from opendbc.car import structs
+from abc import ABC, abstractmethod
+from opendbc.sunnypilot.car.hyundai.values import HyundaiFlagsSP
+
+
+class EcuInterceptorBase(ABC):
+  def __init__(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP):
+    self.CP = CP
+    self.CP_SP = CP_SP
+    self.car_state = None
+
+  @property
+  def enabled(self):
+    return self.CP_SP.flags & HyundaiFlagsSP.ENHANCED_SCC
+
+  @property
+  @abstractmethod
+  def trigger_msg(self):
+    pass
+
+  def update_car_state(self, car_state):
+    """
+      This method is invoked by the CarController to update the car state on the ESCC object.
+      The updated state is then used to update SCC12 with the current car state values received through ESCC.
+      :param car_state:
+      :return:
+    """
+    self.car_state = car_state
+
+  def create_adas_drv_intercept_msg(self, packer, CAN):
+    values = {
+      "status": self.enabled
+    }
+    return [packer.make_can_msg("ADAS_DRV_INTERCEPT", CAN.ACAN, values)]
+
+
+class EcuInterceptorCarStateBase(ABC):
+  def __init__(self):
+    pass
+
+
+class EcuInterceptorCarControllerBase(ABC):
+  def __init__(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP):
+    self.interceptor = self.initialize(CP, CP_SP)
+
+  @abstractmethod
+  def initialize(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP) -> EcuInterceptorBase:
+    pass
+
+  def update(self, car_state):
+    self.interceptor.update_car_state(car_state)
+
+
+class EcuInterceptorRadarInterfaceBase(ABC):
+  def __init__(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP):
+    self.interceptor = self.initialize(CP, CP_SP)
+    self.use_interceptor = False
+
+  @abstractmethod
+  def initialize(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP) -> EcuInterceptorBase:
+    """
+      This method is invoked to initialize the ESCC object.
+      It should be called before any other methods are invoked.
+    """
+    pass
