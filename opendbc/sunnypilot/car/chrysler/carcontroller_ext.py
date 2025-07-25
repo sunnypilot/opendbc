553f34c2817c6aa67136e1bb9b00f68dd50eaf4a
Jason Wen <haibin.wen3@gmail.com>
2025-07-25 11:46:03 -0400

    FCA: Steer to Zero module support

--- opendbc/sunnypilot/car/chrysler/carcontroller_ext.py
+++ opendbc/sunnypilot/car/chrysler/carcontroller_ext.py
@@ -1,16 +1,21 @@
 from opendbc.car import structs
 from opendbc.car.interfaces import CarStateBase
 from opendbc.car.chrysler.values import RAM_DT
+from opendbc.sunnypilot.car.chrysler.values import ChryslerFlagsSP
+from opendbc_repo.opendbc.safety.tests.test_subaru import lkas_tx_msgs
 
 GearShifter = structs.CarState.GearShifter
 
 
 class CarControllerExt:
-  def __init__(self, CP: structs.CarParams):
+  def __init__(self, CP: structs.CarParams, CP_SP: structs.CarParamsSP):
     self.CP = CP
+    self.CP_SP = CP_SP
 
-  def get_lkas_control_bit(self, CS: CarStateBase, lkas_control_bit: bool, lkas_control_bit_prev: bool) -> bool:
-    if self.CP.carFingerprint in RAM_DT:
+  def get_lkas_control_bit(self, CS: CarStateBase, lkas_active: bool, lkas_control_bit: bool, lkas_control_bit_prev: bool) -> bool:
+    if self.CP_SP.flags & ChryslerFlagsSP.NO_MIN_STEERING_SPEED:
+      lkas_control_bit = lkas_active
+    elif self.CP.carFingerprint in RAM_DT:
       lkas_control_bit = lkas_control_bit_prev
       if self.CP.minEnableSpeed <= CS.out.vEgo <= self.CP.minEnableSpeed + 0.5:
         lkas_control_bit = True
