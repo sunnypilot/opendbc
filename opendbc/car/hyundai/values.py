fd8d6b26a27b03b23c3a26cd1d822489419d7f64
royjr <royjr96@gmail.com>
2025-08-13 15:46:38 -0400

    HYUNDAI_IONIQ_5_N is SCC

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -380,7 +380,7 @@ class CAR(Platforms):
     flags=HyundaiFlags.EV,
   )
   HYUNDAI_IONIQ_5_N = HyundaiCanFDPlatformConfig(
-    [HyundaiCarDocs("Hyundai Ioniq 5 N (with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_m]))],
+    [HyundaiCarDocs("Hyundai Ioniq 5 N (with HDA II) 2024", car_parts=CarParts.common([CarHarness.hyundai_m]))],
     CarSpecs(mass=2205, wheelbase=3, steerRatio=12.70),
     flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE,
   )
