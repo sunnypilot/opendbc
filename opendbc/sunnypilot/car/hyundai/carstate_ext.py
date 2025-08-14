35da1dc1fd90f9b59fb220f23fd1d9015ce695ba
Jason Wen <haibin.wen3@gmail.com>
2025-08-14 04:33:13 -0400

    just hev

--- opendbc/sunnypilot/car/hyundai/carstate_ext.py
+++ opendbc/sunnypilot/car/hyundai/carstate_ext.py
@@ -27,19 +27,12 @@ class CarStateExt:
     self.aBasis = cp.vl["TCS13"]["aBasis"]
 
     if self.CP_SP.flags & HyundaiFlagsSP.NON_SCC:
-      cruise_available_msg = "E_CRUISE_CONTROL" if self.CP.flags & HyundaiFlags.HYBRID else \
-                             "EMS16"
+      cruise_available_msg = "E_CRUISE_CONTROL"
       cruise_available_sig = "CRUISE_LAMP_M"
-
-      cruise_enabled_msg = "E_CRUISE_CONTROL" if self.CP.flags & HyundaiFlags.HYBRID else \
-                           "LVR12"
+      cruise_enabled_msg = "E_CRUISE_CONTROL"
       cruise_enabled_sig = "CF_Lvr_CruiseSet"
-
-      cruise_speed_msg = "ELECT_GEAR" if self.CP.flags & HyundaiFlags.HYBRID else \
-                         "LVR12"
-      cruise_speed_sig = "SLC_SET_SPEED" if self.CP.flags & HyundaiFlags.HYBRID else \
-                         "CF_Lvr_CruiseSet"
-
+      cruise_speed_msg = "ELECT_GEAR"
+      cruise_speed_sig = "SLC_SET_SPEED"
       ret.cruiseState.available = cp.vl[cruise_available_msg][cruise_available_sig] != 0
       ret.cruiseState.enabled = cp.vl[cruise_enabled_msg][cruise_enabled_sig] != 0
       ret.cruiseState.speed = cp.vl[cruise_speed_msg][cruise_speed_sig] * speed_conv
