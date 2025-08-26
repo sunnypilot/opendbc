a609c136da3346f204defccc92a68d2c2834f77c
Jason Wen <haibin.wen3@gmail.com>
2025-08-25 22:24:50 -0400

    duplicated rx check

--- opendbc/safety/modes/gm.h
+++ opendbc/safety/modes/gm.h
@@ -34,10 +34,6 @@ static void gm_rx_hook(const CANPacket_t *msg) {
   const int GM_STANDSTILL_THRSLD = 10;  // 0.311kph
 
   if (msg->bus == 0U) {
-    // Update cruise main (ACC main) state from ECM status for all GM modes
-    if (msg->addr == 0xC9U) {  // ECMEngineStatus
-      acc_main_on = GET_BIT(msg, 29U);
-    }
     if (msg->addr == 0x184U) {
       int torque_driver_new = ((msg->data[6] & 0x7U) << 8) | msg->data[7];
       torque_driver_new = to_signed(torque_driver_new, 11);
