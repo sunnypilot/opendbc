c33efc1199da0e6284587cd935178c8d3f930788
Jason Wen <haibin.wen3@gmail.com>
2025-08-26 21:49:25 -0400

    disable safety checks for now

--- opendbc/safety/modes/subaru_common.h
+++ opendbc/safety/modes/subaru_common.h
@@ -16,6 +16,7 @@ void subaru_common_init(void) {
   subaru_stop_and_go = GET_FLAG(current_safety_param_sp, SUBARU_PARAM_SP_STOP_AND_GO);
 }
 
+/*
 bool subaru_common_stop_and_go_throttle_check(const int throttle_pedal) {
   bool violation = throttle_pedal != 5U || !controls_allowed || vehicle_moving;
   return violation;
@@ -26,3 +27,4 @@ bool subaru_common_stop_and_go_brake_pedal_check(const int speed, const bool is_
   bool violation = speed != val || !controls_allowed || vehicle_moving;
   return violation;
 }
+*/
