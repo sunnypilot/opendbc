f0b1a88b69935eed31c2fdbd253cfd5547aec283
Jason Wen <haibin.wen3@gmail.com>
2025-08-30 20:42:19 -0400

    treat all as camera for now

--- opendbc/car/gm/interface.py
+++ opendbc/car/gm/interface.py
@@ -7,7 +7,7 @@ from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.gm.carcontroller import CarController
 from opendbc.car.gm.carstate import CarState
 from opendbc.car.gm.radar_interface import RadarInterface, RADAR_HEADER_MSG, CAMERA_DATA_HEADER_MSG
-from opendbc.car.gm.values import CAR, CarControllerParams, EV_CAR, CAMERA_ACC_CAR, SDGM_CAR, ALT_ACCS, CanBus, GMSafetyFlags
+from opendbc.car.gm.values import CAR, CarControllerParams, EV_CAR, CAMERA_ACC_CAR, SDGM_CAR, ALT_ACCS, CanBus, GMSafetyFlags, NO_ACC_CAR
 from opendbc.car.interfaces import CarInterfaceBase, TorqueFromLateralAccelCallbackType, LateralAccelFromTorqueCallbackType
 
 from opendbc.sunnypilot.car.gm.interface_ext import CarInterfaceExt
@@ -115,7 +115,9 @@ class CarInterface(CarInterfaceBase, CarInterfaceExt):
 
     ret.longitudinalTuning.kiBP = [5., 35.]
 
-    if candidate in (CAMERA_ACC_CAR | SDGM_CAR):
+    # FIXME-SP: investigate NO_ACC_CAR configs before merging
+    #           this should not be done here if possible
+    if candidate in (CAMERA_ACC_CAR | SDGM_CAR | NO_ACC_CAR):
       ret.alphaLongitudinalAvailable = candidate not in SDGM_CAR
       ret.networkLocation = NetworkLocation.fwdCamera
       ret.radarUnavailable = True  # no radar
