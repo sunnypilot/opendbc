1592dae98c6aaba27c8b81b793c7fd449291c776
DevTekVE <devtekve@gmail.com>
2025-09-14 10:18:59 +0200

    Using the param store properly for the is metric

--- opendbc/sunnypilot/car/intelligent_cruise_button_management_interface_base.py
+++ opendbc/sunnypilot/car/intelligent_cruise_button_management_interface_base.py
@@ -4,7 +4,7 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
-# from opendbc.sunnypilot.car import get_param
+from opendbc.sunnypilot.car import get_param
 
 
 class IntelligentCruiseButtonManagementInterfaceBase:
@@ -16,6 +16,6 @@ class IntelligentCruiseButtonManagementInterfaceBase:
     self.frame = 0
     self.last_button_frame = 0
 
-  # @property
-  # def is_metric(self):
-  #   return int(get_param(self.CP_SP.param, "IsMetric")) == 1
+  @property
+  def is_metric(self):
+    return self.CC_SP.params is not None and get_param(self.CC_SP.params, "IsMetric", False)
