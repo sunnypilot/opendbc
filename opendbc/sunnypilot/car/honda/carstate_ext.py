411e1a423da8889df8614fcdaa7ece8379bd073a
mvl-boston <manuel@alumni.virginia.edu>
2025-09-16 22:28:06 -0400

    Typo fix

--- opendbc/sunnypilot/car/honda/carstate_ext.py
+++ opendbc/sunnypilot/car/honda/carstate_ext.py
@@ -23,5 +23,5 @@ class CarStateExt:
     if self.CP_SP.flags & HondaFlagsSP.CLARITY:
       ret.accFaulted = bool(cp.vl["HYBRID_BRAKE_ERROR"]["BRAKE_ERROR_1"] or cp.vl["HYBRID_BRAKE_ERROR"]["BRAKE_ERROR_2"])
       ret.stockAeb = bool(cp_cam.vl["BRAKE_COMMAND"]["AEB_REQ_1"] and cp_cam.vl["BRAKE_COMMAND"]["COMPUTER_BRAKE_ALT"] > 1e-5)
-      ret.brakeHoldActive = cp.vl[BRAKE_HOLD_HYBRID_ALT]["BRAKE_HOLD_ACTIVE"] == 1
+      ret.brakeHoldActive = cp.vl["BRAKE_HOLD_HYBRID_ALT"]["BRAKE_HOLD_ACTIVE"] == 1
       ret.blockPcmEnable = ret.brakeHoldActive
