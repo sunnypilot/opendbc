65bf249cfb5f80e4c8b4060f4b3bb249f56f0d35
Jason Wen <haibin.wen3@gmail.com>
2025-09-26 23:25:36 -0400

    recheck openpilot long availability

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -212,6 +212,11 @@ class CarInterface(CarInterfaceBase):
       ret.safetyParam |= ToyotaSafetyFlagsSP.GAS_INTERCEPTOR
       stock_cp.minEnableSpeed = -1.
 
+    if not stock_cp.openpilotLongitudinalControl:
+      stock_cp.safetyConfigs[0].safetyParam |= ToyotaSafetyFlags.STOCK_LONGITUDINAL.value
+    else:
+      stock_cp.safetyConfigs[0].safetyParam &= ~ToyotaSafetyFlags.STOCK_LONGITUDINAL.value
+
     return ret
 
   @staticmethod
