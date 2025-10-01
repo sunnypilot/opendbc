e81a46c0253e1c7541fe6f11e858baa0f8c67bc6
Jason Wen <haibin.wen3@gmail.com>
2025-10-01 00:52:26 -0400

    no longer applies

--- opendbc/car/subaru/values.py
+++ opendbc/car/subaru/values.py
@@ -19,7 +19,6 @@ class CarControllerParams:
     self.STEER_DRIVER_FACTOR = 1       # from dbc
 
     if CP.flags & SubaruFlags.GLOBAL_GEN2:
-      # TODO: lower rate limits, this reaches min/max in 0.5s which negatively affects tuning
       self.STEER_MAX = 1600
       self.STEER_DELTA_UP = 40
       self.STEER_DELTA_DOWN = 55
