07e3b0d243ba26c5fb8b1c5c871e130a234e6c12
Jason Wen <haibin.wen3@gmail.com>
2025-10-02 13:59:43 -0400

    move to a single helper

--- opendbc/sunnypilot/car/fingerprints_ext.py
+++ opendbc/sunnypilot/car/fingerprints_ext.py
@@ -6,6 +6,25 @@ See the LICENSE.md file in the root directory for more details.
 """
 
 
+def merge_fw_versions(fw_versions, new_fw_versions):
+  """
+    Merge firmware versions by extending lists for matching ECUs,
+    adding all entries regardless of duplicates.
+  """
+  for c, f in new_fw_versions.items():
+    if c not in fw_versions:
+      fw_versions[c] = f
+      continue
+
+    for e, new_fw_list in f.items():
+      if e not in fw_versions[c]:
+        fw_versions[c][e] = new_fw_list
+      else:
+        fw_versions[c][e].extend(new_fw_list)
+
+  return fw_versions
+
+
 def merge_fingerprints(fingerprints, new_fingerprints):
   """
     Merge fingerprints by extending lists for matching keys,
