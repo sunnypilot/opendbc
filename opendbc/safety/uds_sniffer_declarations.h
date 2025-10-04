61363a3ceb75e7e22194254c2939c75dd6e049f3
DevTekVE <devtekve@gmail.com>
2025-10-04 12:47:47 +0200

    misra

--- opendbc/safety/uds_sniffer_declarations.h
+++ opendbc/safety/uds_sniffer_declarations.h
@@ -76,7 +76,11 @@ static uint8_t get_isotp_frame_type(const CANPacket_t *msg);
 static uint16_t get_isotp_data_length(const CANPacket_t *msg);
 
 extern uint32_t get_ts_elapsed(uint32_t ts, uint32_t ts_last);
-#ifndef __APPLE__
+
+#ifndef memcpy
 extern void *memcpy(void *dest, const void *src, unsigned int len);
+#endif
+
+#ifndef memset
 extern void *memset(void *str, int c, unsigned int n);
 #endif
