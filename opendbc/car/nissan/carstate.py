54093cd23c8d0a0338e6964c2d082cf936e8aa1e
downquark7 <mail2emn@gmail.com>
2025-10-07 17:57:03 -0500

    Refactor Nissan ICBM button handling: centralize `create_cruise_button_msg`, add `SET` and `RES` button support, and improve button event processing in `CarState`.

--- opendbc/car/nissan/carstate.py
+++ opendbc/car/nissan/carstate.py
@@ -22,7 +22,10 @@ class CarState(CarStateBase):
     self.steeringTorqueSamples = deque(TORQUE_SAMPLES*[0], TORQUE_SAMPLES)
     self.shifter_values = can_define.dv["GEARBOX"]["GEAR_SHIFTER"]
 
+    # Cruise buttons
     self.distance_button = 0
+    self.set_button = 0
+    self.res_button = 0
 
   def update(self, can_parsers) -> tuple[structs.CarState, structs.CarStateSP]:
     cp = can_parsers[Bus.pt]
@@ -33,7 +36,12 @@ class CarState(CarStateBase):
     ret_sp = structs.CarStateSP()
 
     prev_distance_button = self.distance_button
+    prev_set_button = self.set_button
+    prev_res_button = self.res_button
+
     self.distance_button = cp.vl["CRUISE_THROTTLE"]["FOLLOW_DISTANCE_BUTTON"]
+    self.set_button = cp.vl["CRUISE_THROTTLE"].get("SET_BUTTON", 0)
+    self.res_button = cp.vl["CRUISE_THROTTLE"].get("RES_BUTTON", 0)
 
     if self.CP.carFingerprint in (CAR.NISSAN_ROGUE, CAR.NISSAN_XTRAIL, CAR.NISSAN_ALTIMA):
       ret.gasPressed = bool(cp.vl["GAS_PEDAL"]["GAS_PEDAL"] > 3)
@@ -127,7 +135,11 @@ class CarState(CarStateBase):
       self.lkas_hud_msg = copy.copy(cp_adas.vl["PROPILOT_HUD"])
       self.lkas_hud_info_msg = copy.copy(cp_adas.vl["PROPILOT_HUD_INFO_MSG"])
 
-    ret.buttonEvents = create_button_events(self.distance_button, prev_distance_button, {1: ButtonType.gapAdjustCruise})
+    ret.buttonEvents = [
+      *create_button_events(self.res_button, prev_res_button, {1: ButtonType.accelCruise}),
+      *create_button_events(self.set_button, prev_set_button, {1: ButtonType.decelCruise}),
+      *create_button_events(self.distance_button, prev_distance_button, {1: ButtonType.gapAdjustCruise}),
+    ]
 
     return ret, ret_sp
 
