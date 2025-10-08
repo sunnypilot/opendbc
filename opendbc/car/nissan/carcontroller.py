883af3371e54005708e6c5e2fe581bf62e906dae
downquark7 <mail2emn@gmail.com>
2025-10-07 23:32:22 -0500

    Refactor Nissan CAN message creation to support Leaf-specific fields and implement Mazda-style resume behavior for CRUISE_THROTTLE messages

--- opendbc/car/nissan/carcontroller.py
+++ opendbc/car/nissan/carcontroller.py
@@ -71,6 +71,11 @@ class CarController(CarControllerBase, IntelligentCruiseButtonManagementInterfac
           self.packer, CS.lkas_hud_info_msg, steer_hud_alert
         ))
 
+    # Resume from standstill like Mazda: send RES button when planner requests resume
+    if CC.cruiseControl.resume and self.frame % 5 == 0:
+      base_counter = CS.cruise_throttle_msg.get("COUNTER", 0)
+      can_sends.append(nissancan.create_cruise_button_msg(self.packer, self.car_fingerprint, CS.cruise_throttle_msg, "RES_BUTTON", base_counter))
+
     # Intelligent Cruise Button Management
     can_sends.extend(IntelligentCruiseButtonManagementInterface.update(self, CS, CC_SP, self.packer, self.frame, self.last_button_frame))
 
