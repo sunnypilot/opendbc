2fce39c3e5cdbb1eeaf97ef5637b89f20a7faddd
MVL <manuel@alumni.virginia.edu>
2025-10-09 00:08:58 -0400

    generalize Clarity to Nidec Hybrid

--- opendbc/sunnypilot/car/honda/values_ext.py
+++ opendbc/sunnypilot/car/honda/values_ext.py
@@ -9,10 +9,10 @@ from enum import IntFlag
 
 
 class HondaFlagsSP(IntFlag):
-  CLARITY = 1
+  NIDEC_HYBRID = 1
   EPS_MODIFIED = 2
 
 
 class HondaSafetyFlagsSP:
-  CLARITY = 1
+  NIDEC_HYBRID = 1
   GAS_INTERCEPTOR = 2
