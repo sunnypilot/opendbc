a9aa9eb4b6ddfdec5e0cb300fdd7599d4a82d10a
MVL <manuel@alumni.virginia.edu>
2025-10-09 19:14:02 -0400

    Add additional Accord 9G Hybrid fingerprint, remove combinationmeter

--- opendbc/sunnypilot/car/honda/fingerprints_ext.py
+++ opendbc/sunnypilot/car/honda/fingerprints_ext.py
@@ -56,18 +56,20 @@ FW_VERSIONS_EXT = {
       b'36161-TRW-A110\x00\x00',
     ],
   },
-  CAR.HONDA_ACCORD_4CYL_9TH_GEN: {
+  CAR.HONDA_ACCORD_9G: {
     (Ecu.vsa, 0x18DA28F1, None): [
       b'57114-T2F-X840\x00\x00',
     ],
     (Ecu.fwdRadar, 0x18DAB0F1, None): [
       b'36161-T2F-A140\x00\x00',
-    ],
-    (Ecu.combinationMeter, 0x18DA60F1, None): [
-      b'78109-T2F-L110\x00\x00',
+      b'36161-T3Z-A830\x00\x00'
     ],
     (Ecu.srs, 0x18DA53F1, None): [
       b'77959-T2F-A030\x00\x00',
+      b'77959-T3Z-A020\x00\x00',
+    ],
+    (Ecu.gateway, 0x18DAEFF1, None): [
+      b'38897-T3W-0130\x00\x00',
     ],
   },
 }
