dfe73e276bf942f770f468728f008ac37de65996
downquark7 <mail2emn@gmail.com>
2025-10-08 20:21:35 -0500

    Refactor Nissan cruise control logic for accurate counter handling and improved consistency in button message creation.

--- opendbc/sunnypilot/car/nissan/icbm.py
+++ opendbc/sunnypilot/car/nissan/icbm.py
@@ -5,7 +5,7 @@ This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
 
-from opendbc.car import DT_CTRL, structs
+from opendbc.car import structs
 from opendbc.car.can_definitions import CanData
 from opendbc.sunnypilot.car.intelligent_cruise_button_management_interface_base import IntelligentCruiseButtonManagementInterfaceBase
 from opendbc.car.nissan.nissancan import create_cruise_button_msg
@@ -23,6 +23,7 @@ BUTTON_FIELDS = {
 class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManagementInterfaceBase):
   def __init__(self, CP, CP_SP):
     super().__init__(CP, CP_SP)
+    self._last_cruise_throttle_counter = None
 
   def update(self, CS, CC_SP, packer, frame, last_button_frame) -> list[CanData]:
     can_sends: list[CanData] = []
@@ -31,13 +32,18 @@ class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManageme
     self.frame = frame
     self.last_button_frame = last_button_frame
 
-    if self.ICBM.sendButton != SendButtonState.none:
-      send_field = BUTTON_FIELDS[self.ICBM.sendButton]
+    current_counter = CS.cruise_throttle_msg.get("COUNTER", 0)
+    should_send_on_increase = (
+      self._last_cruise_throttle_counter is not None and current_counter > self._last_cruise_throttle_counter
+    )
 
-      if (self.frame - self.last_button_frame) * DT_CTRL > 0.1:
-          counter = (CS.cruise_throttle_msg.get("COUNTER", 0) + 1) % 4
-          can_sends.append(create_cruise_button_msg(packer, self.CP.carFingerprint, CS.cruise_throttle_msg, send_field, counter))
-          if (self.frame - self.last_button_frame) * DT_CTRL > 0.2:
-            self.last_button_frame = self.frame
+    if self.ICBM.sendButton != SendButtonState.none and should_send_on_increase:
+      send_field = BUTTON_FIELDS[self.ICBM.sendButton]
+      # Keep the counter the same as car-provided; no modifications or offsets
+      can_sends.append(create_cruise_button_msg(packer, self.CP.carFingerprint, CS.cruise_throttle_msg, send_field))
+
+    # Update stored counter only when it increases
+    if self._last_cruise_throttle_counter is None or current_counter > self._last_cruise_throttle_counter:
+      self._last_cruise_throttle_counter = current_counter
 
     return can_sends
