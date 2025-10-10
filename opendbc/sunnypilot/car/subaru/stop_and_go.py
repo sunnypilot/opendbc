d87d3600e380853141283ccd2687eb35001e80a6
Jason Wen <haibin.wen3@gmail.com>
2025-10-10 11:07:55 -0400

    Revert "longer wait before sending"
    This reverts commit 74f80ecc0fdb091fb042750b8c7dc143b0121e05.

--- opendbc/sunnypilot/car/subaru/stop_and_go.py
+++ opendbc/sunnypilot/car/subaru/stop_and_go.py
@@ -68,7 +68,7 @@ class SnGCarController:
       self.last_standstill_frame = frame
 
     # Check if we've been in standstill long enough
-    mpb_standstill_timers = (1.0, 1.05) if self.CP.flags & SubaruFlags.PREGLOBAL else (0.5, 0.55)
+    mpb_standstill_timers = (0.75, 0.8) if self.CP.flags & SubaruFlags.PREGLOBAL else (0.5, 0.55)
     standstill_duration = (frame - self.last_standstill_frame) * DT_CTRL
     in_standstill_hold = standstill_duration > mpb_standstill_timers[0]
     if (frame - self.last_standstill_frame) * DT_CTRL >= mpb_standstill_timers[1]:
