608ac9fce993108295c749436e332bc4d49222a1
Micim987 <4733781+Micim987@users.noreply.github.com>
2025-10-10 06:59:55 -0400

    Refactor gas calculation in GasInterceptorCarController to remove aggressive speed multiplier

--- opendbc/sunnypilot/car/honda/gas_interceptor.py
+++ opendbc/sunnypilot/car/honda/gas_interceptor.py
@@ -25,14 +25,12 @@ class GasInterceptorCarController:
     can_sends = []
 
     if self.CP_SP.enableGasInterceptor:
-      # way too aggressive at low speed without this
-      gas_mult = np.interp(CS.out.vEgo, [0., 10.], [0.4, 1.0])
       # send exactly zero if apply_gas is zero. Interceptor will send the max between read value and apply_gas.
       # This prevents unexpected pedal range rescaling
       # Sending non-zero gas when OP is not enabled will cause the PCM not to respond to throttle as expected
       # when you do enable.
       if CC.longActive:
-        self.gas = float(np.clip(gas_mult * (gas - brake + wind_brake * 3 / 4), 0., 1.))
+        self.gas = float(np.clip(gas - brake + wind_brake * 3 / 4), 0., 1.)
       else:
         self.gas = 0.0
       can_sends.append(create_gas_interceptor_command(packer, self.gas, frame // 2))
