d499191dc8c42bb201376225d1574652ae59c37c
Jason Wen <haibin.wen3@gmail.com>
2025-10-10 11:07:49 -0400

    Revert "about to cry"
    This reverts commit eaea0eec60005a6bf6a2c5a405b20cc026f266d2.

--- opendbc/sunnypilot/car/subaru/stop_and_go.py
+++ opendbc/sunnypilot/car/subaru/stop_and_go.py
@@ -68,10 +68,10 @@ class SnGCarController:
       self.last_standstill_frame = frame
 
     # Check if we've been in standstill long enough
-    mpb_standstill_timers = (0.5, 0.0) if self.CP.flags & SubaruFlags.PREGLOBAL else (0.5, 0.55)
+    mpb_standstill_timers = (0.6, 0.6) if self.CP.flags & SubaruFlags.PREGLOBAL else (0.5, 0.55)
     standstill_duration = (frame - self.last_standstill_frame) * DT_CTRL
     in_standstill_hold = standstill_duration > mpb_standstill_timers[0]
-    if mpb_standstill_timers[1] != 0.0 and standstill_duration >= mpb_standstill_timers[1]:
+    if standstill_duration >= mpb_standstill_timers[1]:
       self.last_standstill_frame = frame
 
     # Car state distance-based conditions (EPB only)
