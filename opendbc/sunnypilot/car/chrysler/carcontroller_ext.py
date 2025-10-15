d7add4ebdcb8795c3722edbacd659e1676f5dffb
Jason Wen <haibin.wen3@gmail.com>
2025-10-15 00:34:56 -0400

    Partial Revert "FCA: Ram 1500 improvements (#797)"

--- opendbc/sunnypilot/car/chrysler/carcontroller_ext.py
+++ opendbc/sunnypilot/car/chrysler/carcontroller_ext.py
@@ -14,11 +14,5 @@ class CarControllerExt:
   def get_lkas_control_bit(self, CS: CarStateBase, CC: structs.CarControl, lkas_control_bit: bool, lkas_control_bit_prev: bool) -> bool:
     if self.CP_SP.flags & ChryslerFlagsSP.NO_MIN_STEERING_SPEED:
       lkas_control_bit = CC.latActive
-    elif self.CP.carFingerprint in RAM_DT:
-      lkas_control_bit = lkas_control_bit_prev
-      if self.CP.minEnableSpeed <= CS.out.vEgo <= self.CP.minEnableSpeed + 0.5:
-        lkas_control_bit = True
-      if CS.out.gearShifter != GearShifter.drive:
-        lkas_control_bit = False
 
     return lkas_control_bit
