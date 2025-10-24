323c2b475a30e7a191ec03948487ae2530531a00
MVL <manuel@alumni.virginia.edu>
2025-10-24 08:12:39 -0400

    Adding 2019 ICE fingerprints

--- opendbc/sunnypilot/car/honda/fingerprints_ext.py
+++ opendbc/sunnypilot/car/honda/fingerprints_ext.py
@@ -59,11 +59,13 @@ FW_VERSIONS_EXT = {
   CAR.ACURA_MDX_3G: {
     (Ecu.vsa, 0x18da28f1, None): [
       b'57114-TRX-H130\x00\x00',
+      b'57114-TYS-A340\x00\x00',
       b'57114-TZ6-A910\x00\x00',
     ],
     (Ecu.fwdRadar, 0x18dab0f1, None): [
-      b'36161-TZ6-A730\x00\x00',
       b'36161-TRX-A820\x00\x00',
+      b'36161-TYS-A020\x00\x00',
+      b'36161-TZ6-A730\x00\x00',
     ],
     (Ecu.shiftByWire, 0x18da0bf1, None): [
       b'54008-TRX-A710\x00\x00',
@@ -79,6 +81,7 @@ FW_VERSIONS_EXT = {
       b'38897-TRX-A220\x00\x00',
     ],
     (Ecu.transmission, 0x18da1ef1, None): [
+      b'28101-5DH-A330\x00\x00',
       b'28101-5NC-A310\x00\x00',
       b'28101-5NC-A770\x00\x00',
     ],
