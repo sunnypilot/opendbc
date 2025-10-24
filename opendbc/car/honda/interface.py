128e36a8005cec6b27978b261819be242c5f224b
Jason Wen <haibin.wen3@gmail.com>
2025-10-24 16:51:36 -0400

    wrong

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -297,7 +297,7 @@ class CarInterface(CarInterfaceBase):
 
     stock_cp.autoResumeSng = stock_cp.autoResumeSng or ret.enableGasInterceptor
 
-    ret.intelligentCruiseButtonManagementAvailable = candidate in (HONDA_BOSCH - HONDA_BOSCH_CANFD) and \
+    ret.intelligentCruiseButtonManagementAvailable = candidate in (HONDA_BOSCH - HONDA_BOSCH_CANFD) or \
                                                      (candidate in HONDA_BOSCH_CANFD and not is_release_sp)
 
     return ret
