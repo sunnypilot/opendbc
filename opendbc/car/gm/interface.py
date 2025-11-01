be0a4e17748c0c47dc6058aec1d2a8f0e539fdf9
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-11-01 14:17:53 -0500

    Update interface.py

--- opendbc/car/gm/interface.py
+++ opendbc/car/gm/interface.py
@@ -242,6 +242,8 @@ class CarInterface(CarInterfaceBase):
       ret.pcmCruise = False
       ret.openpilotLongitudinalControl = True
       ret.autoResumeSng = True
+      ret.safetyConfigs[0].safetyParam |= GMSafetyFlagsSP.GAS_INTERCEPTOR.value
+      ret.safetyConfigs[0].safetyParam |= GMSafetyFlagsSP.PEDAL_LONG.value
 
       # Pedal interceptor tuning
       ret.longitudinalTuning.kiBP = [0., 3., 6., 35.]
