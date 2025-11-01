35fe7a86b6eb731c72f75dcc28f4b43a16bb514e
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-11-01 13:28:46 -0500

    Update carstate_ext.py

--- opendbc/sunnypilot/car/gm/carstate_ext.py
+++ opendbc/sunnypilot/car/gm/carstate_ext.py
@@ -16,6 +16,8 @@ class CarStateExt:
   def __init__(self, CP, CP_SP):
     self.CP = CP
     self.CP_SP = CP_SP
+    self.single_pedal_mode = False
+    self.pedal_steady = 0.
 
   def update(self, ret: structs.CarState, can_parsers: dict[StrEnum, CANParser]) -> None:
     pt_cp = can_parsers[Bus.pt]
@@ -24,3 +26,22 @@ class CarStateExt:
       ret.cruiseState.enabled = pt_cp.vl["ECMCruiseControl"]["CruiseActive"] != 0
       ret.cruiseState.speed = pt_cp.vl["ECMCruiseControl"]["CruiseSetSpeed"] * CV.KPH_TO_MS
       ret.accFaulted = False
+
+  def update_gas_pressed(self, ret: structs.CarState, pt_cp) -> structs.CarState:
+    """Update gas pressed for pedal interceptor vehicles"""
+    from opendbc.car.gm.values import CAMERA_ACC_CAR
+    if self.CP.enableGasInterceptorDEPRECATED:
+      gas = (pt_cp.vl["GAS_SENSOR"]["INTERCEPTOR_GAS"] + pt_cp.vl["GAS_SENSOR"]["INTERCEPTOR_GAS2"]) / 2.
+      # Panda 515 threshold = 10.88. Set lower to avoid panda blocking messages and GasInterceptor faulting.
+      threshold = 20 if self.CP.carFingerprint in CAMERA_ACC_CAR else 4
+      ret.gasPressed = gas > threshold
+    else:
+      ret.gasPressed = pt_cp.vl["AcceleratorPedal2"]["AcceleratorPedal2"] / 254. > 1e-5
+    return ret
+
+  def update_regen_braking(self, ret: structs.CarState, pt_cp) -> structs.CarState:
+    """Update regen braking for pedal interceptor vehicles"""
+    if self.CP.transmissionType == structs.CarParams.TransmissionType.direct:
+      ret.regenBraking = pt_cp.vl["EBCMRegenPaddle"]["RegenPaddle"] != 0
+      self.single_pedal_mode = ret.gearShifter == structs.CarState.GearShifter.low or pt_cp.vl["EVDriveMode"]["SinglePedalModeActive"] == 1
+    return ret
