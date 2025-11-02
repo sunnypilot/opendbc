f9064759891601c0accc62fe9050bee41212f765
roenthomas <43324106+roenthomas@users.noreply.github.com>
2025-11-02 15:55:36 -0500

    Implemented feedback from PR discussion
    Reduced to 2 digit trailing year, mass in kg, min_steer_speed=3 per uploaded route.

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -349,8 +349,8 @@ class CAR(Platforms):
 
   # port extensions
   HONDA_CLARITY = HondaNidecPlatformConfig(
-    [HondaCarDocs("Honda Clarity 2018-2021", "All", min_steer_speed=3. * CV.MPH_TO_MS)],
-    CarSpecs(mass=4052. * CV.LB_TO_KG, wheelbase=2.75, centerToFrontRatio=0.4, steerRatio=16.50),
+    [HondaCarDocs("Honda Clarity 2018-21", "All", min_steer_speed=3. * CV.MPH_TO_MS)],
+    CarSpecs(mass=1838, wheelbase=2.75, centerToFrontRatio=0.4, steerRatio=16.5),
     radar_dbc_dict('honda_clarity_hybrid_2018_can_generated'),
     flags=HondaFlags.HAS_ALL_DOOR_STATES,
   )
