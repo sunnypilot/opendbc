cb4946a3c7b31b9b3a9c4a39529cc9f82a2e48c3
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-11-03 16:56:45 -0600

    mybe this?

--- opendbc/safety/modes/gm.h
+++ opendbc/safety/modes/gm.h
@@ -116,9 +116,13 @@ static void gm_rx_hook(const CANPacket_t *msg) {
       acc_main_on = GET_BIT(msg, 29U);
     }
 
-    if (msg->addr == 0x3D1U && !gm_non_acc) {
+    if (msg->addr == 0x3D1U) {
       bool cruise_engaged = GET_BIT(msg, 39U);
-      pcm_cruise_check(cruise_engaged);
+      if (!gm_non_acc) {
+        pcm_cruise_check(cruise_engaged);
+      } else {
+        cruise_engaged_prev = cruise_engaged;
+      }
     }
   }
 }
