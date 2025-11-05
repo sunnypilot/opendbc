26df8539772abe86dd3651a202c425593d273eb6
downquark7 <mail2emn@gmail.com>
2025-11-05 04:49:56 -0600

    add state machine and button queue

--- opendbc/sunnypilot/car/nissan/icbm.py
+++ opendbc/sunnypilot/car/nissan/icbm.py
@@ -3,11 +3,12 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, downquark7, and a number of other c
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
-
 from opendbc.car import DT_CTRL, structs
 from opendbc.car.can_definitions import CanData
 from opendbc.sunnypilot.car.intelligent_cruise_button_management_interface_base import IntelligentCruiseButtonManagementInterfaceBase
 from opendbc.car.nissan import nissancan
+
+from collections import deque
 
 ButtonType = structs.CarState.ButtonEvent.Type
 SendButtonState = structs.IntelligentCruiseButtonManagement.SendButtonState
@@ -21,8 +22,10 @@ BUTTONS = {
 class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManagementInterfaceBase):
   def __init__(self, CP, CP_SP):
     super().__init__(CP, CP_SP)
-    self.queue_button = None
-    self.send_button = None
+    self.__state = 0
+    self.__queued_buttons = deque()
+    self.__distance_button = False
+    self.__prev_distance_button = False
 
   def update(self, CS, CC_SP, packer, frame, last_button_frame) -> list[CanData]:
     can_sends: list[CanData] = []
@@ -31,25 +34,36 @@ class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManageme
     self.frame = frame
     self.last_button_frame = last_button_frame
 
-    # may add more buttons to this handler later
-    # always need to queue manual button presses if icbm isn't sending because this is how we keep track of the timer resets
-    if CS.cruise_throttle_msg["FOLLOW_DISTANCE_BUTTON"]:
-      self.queue_button = "FOLLOW_DISTANCE_BUTTON"
+    self.__prev_distance_button = self.__distance_button
+    self.__distance_button = CS.cruise_throttle_msg["FOLLOW_DISTANCE_BUTTON"]
 
-    if not self.queue_button and self.ICBM.sendButton != SendButtonState.none:
-        self.queue_button = BUTTONS[self.ICBM.sendButton]
+    # record if button press blocked during icbm send cycle to send it later for a consistent user experience
+    # can add other buttons to this if necessary
+    if self.__state in (1, 2):
+      if self.__distance_button and not self.__prev_distance_button:
+        self.__queued_buttons.append("FOLLOW_DISTANCE_BUTTON")
 
-    if (self.frame - self.last_button_frame) * DT_CTRL < 0.07:
+    # before button send state
+    if self.__state == 1:
       can_sends.append(nissancan.create_cruise_throttle_msg(packer, self.CP.carFingerprint, CS.cruise_throttle_msg, self.frame, "NO_BUTTON_PRESSED"))
+      if (self.frame - self.last_button_frame) * DT_CTRL >= 0.07:
+        self.__state = 2
+        return can_sends # prevent sending 2 different messages in the same frame
 
-    elif (self.frame - self.last_button_frame) * DT_CTRL <= 0.11:
-      can_sends.append(nissancan.create_cruise_throttle_msg(packer, self.CP.carFingerprint, CS.cruise_throttle_msg, self.frame, self.send_button))
+    # during button send state
+    if self.__state == 2:
+      can_sends.append(nissancan.create_cruise_throttle_msg(packer, self.CP.carFingerprint, CS.cruise_throttle_msg, self.frame, self.__queued_buttons[0]))
+      if (self.frame - self.last_button_frame) * DT_CTRL >= 0.11:
+        self.__queued_buttons.popleft()
+        self.__state = 0
 
-    if (self.frame - self.last_button_frame) * DT_CTRL >= 0.11:
-      manually_holding_button = (self.queue_button == self.send_button) and not CS.cruise_throttle_msg["NO_BUTTON_PRESSED"]
-      if self.send_button and not manually_holding_button:
-        self.last_button_frame = self.frame
-      self.send_button = self.queue_button
-      self.queue_button = None
+    # idle state
+    if self.__state == 0:
+      self.last_button_frame = self.frame
+      if self.__queued_buttons:
+        self.__state = 1
+      elif self.ICBM.sendButton != SendButtonState.none:
+        self.__queued_buttons.append(self.ICBM.sendButton)
+        self.__state = 1
 
     return can_sends
