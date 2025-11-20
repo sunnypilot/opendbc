34fb3a3385a7c076231bff2b1c457226db43e7cc
Sathans <hans5438@gmail.com>
2025-11-20 19:00:22 +0100

    small-wheel icon

--- opendbc/car/nissan/nissancan.py
+++ opendbc/car/nissan/nissancan.py
@@ -127,7 +127,6 @@ def create_lkas_hud_msg(packer, lkas_hud_msg, enabled, left_line, right_line, le
     "FOLLOW_DISTANCE",
     "AUDIBLE_TONE",
     "SPEED_SET_ICON",
-    "SMALL_STEERING_WHEEL_ICON",
     "unknown59",
     "unknown55",
     "unknown26",
@@ -144,6 +143,7 @@ def create_lkas_hud_msg(packer, lkas_hud_msg, enabled, left_line, right_line, le
   values["LEFT_LANE_YELLOW_FLASH"] = 1 if left_lane_depart else 0
 
   values["LARGE_STEERING_WHEEL_ICON"] = 2 if enabled else 0
+  values["SMALL_STEERING_WHEEL_ICON"] = 3 if enabled else 1
   values["RIGHT_LANE_GREEN"] = 1 if right_line and enabled else 0
   values["LEFT_LANE_GREEN"] = 1 if left_line and enabled else 0
 
