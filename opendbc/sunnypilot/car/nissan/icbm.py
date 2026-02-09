e59568272283e0ae8a3a36b4767216d79a287740
downquark7 <mail2emn@gmail.com>
2026-02-08 20:37:40 -0600

    Add SET and RES button handling to Nissan ICBM button queue logic

--- opendbc/sunnypilot/car/nissan/icbm.py
+++ opendbc/sunnypilot/car/nissan/icbm.py
@@ -26,6 +26,10 @@ class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManageme
     self.__queued_buttons = deque()
     self.__distance_button = False
     self.__prev_distance_button = False
+    self.__set_button = False
+    self.__prev_set_button = False
+    self.__res_button = False
+    self.__prev_res_button = False
 
   def update(self, CS, CC_SP, packer, frame, last_button_frame) -> list[CanData]:
     can_sends: list[CanData] = []
@@ -37,13 +41,26 @@ class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManageme
     self.__prev_distance_button = self.__distance_button
     self.__distance_button = CS.cruise_throttle_msg["FOLLOW_DISTANCE_BUTTON"]
 
+    self.__prev_set_button = self.__set_button
+    self.__set_button = CS.cruise_throttle_msg["SET_BUTTON"]
+
+    self.__prev_res_button = self.__res_button
+    self.__res_button = CS.cruise_throttle_msg["RES_BUTTON"]
+
     # record if button press blocked during icbm send cycle to send it later for a consistent user experience
     # can add other buttons to this if necessary
     if self.__state in (1, 2):
       if self.__distance_button and not self.__prev_distance_button:
         self.__queued_buttons.append("FOLLOW_DISTANCE_BUTTON")
 
-    # before button send state
+      if self.__set_button and not self.__prev_set_button:
+        self.__queued_buttons.append("SET_BUTTON")
+
+      if self.__res_button and not self.__prev_res_button:
+        self.__queued_buttons.append("RES_BUTTON")
+
+
+    # block button sends state
     if self.__state == 1:
       can_sends.append(nissancan.create_cruise_throttle_msg(packer, self.CP.carFingerprint, CS.cruise_throttle_msg, self.frame, "NO_BUTTON_PRESSED"))
       if (self.frame - self.last_button_frame) * DT_CTRL >= 0.06:
@@ -60,7 +77,7 @@ class IntelligentCruiseButtonManagementInterface(IntelligentCruiseButtonManageme
       can_sends.append(nissancan.create_cruise_throttle_msg(packer, self.CP.carFingerprint, CS.cruise_throttle_msg, self.frame, self.__queued_buttons[0]))
       if (self.frame - self.last_button_frame) * DT_CTRL >= 0.12:
         self.__queued_buttons.popleft()
-        self.__state = 0
+        self.__state = 1
         self.last_button_frame = self.frame
 
     # idle state
