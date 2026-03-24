c7877c21d869cc6c3b7615309c1f20ff1308048e
royjr <royjr96@gmail.com>
2026-03-24 14:24:51 -0400

    debug 2

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -274,7 +274,7 @@ def create_spas_messages(packer, CAN, left_blink, right_blink, msg_16a):
   # }
   # ret.append(packer.make_can_msg("SPAS1", CAN.ECAN, values))
 
-  blink = 1
+  blink = 2
   if left_blink:
     blink = 3
   elif right_blink:
