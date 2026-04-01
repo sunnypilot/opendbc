f82caf34c2137ed56525a1feddf18345ca0eb7a8
Jason Wen <haibin.wen3@gmail.com>
2026-03-31 21:49:06 -0400

    match

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -33,7 +33,7 @@ class CarControllerParams:
     ([], []),
     MAX_LATERAL_ACCEL=(ISO_LATERAL_ACCEL + (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_ROAD_ROLL)),  # ~3.6 m/s^2
     MAX_LATERAL_JERK=(3.0 + (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_ROAD_ROLL)),  # ~3.6 m/s^3,
-    MAX_ANGLE_RATE=5  # comfort rate limit for angle commands, in degrees per frame.
+    MAX_ANGLE_RATE=1  # comfort rate limit for angle commands, in degrees per frame.
   )
 
   # Torque control parameters:
