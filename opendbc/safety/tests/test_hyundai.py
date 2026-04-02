341b20edd24f371b425fe24c391a17866ef750ad
Jason Wen <haibin.wen3@gmail.com>
2026-04-02 03:30:21 -0400

    brand new day

--- opendbc/safety/tests/test_hyundai.py
+++ opendbc/safety/tests/test_hyundai.py
@@ -126,7 +126,7 @@ class TestHyundaiSafety(HyundaiButtonBase, common.CarSafetyTest, common.DriverTo
 
   def _speed_msg(self, speed):
     # safety doesn't scale, so undo the scaling
-    values = {"WHL_SPD_%s" % s: speed * 0.03125 for s in ["FL", "FR", "RL", "RR"]}
+    values = {"WHL_SPD_%s" % s: speed * 3.6 for s in ["FL", "FR", "RL", "RR"]}
     values["WHL_SPD_AliveCounter_LSB"] = (self.cnt_speed % 16) & 0x3
     values["WHL_SPD_AliveCounter_MSB"] = (self.cnt_speed % 16) >> 2
     self.__class__.cnt_speed += 1
