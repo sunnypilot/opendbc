b0f35543082d539e4a0d2ab5c1b26e14463c99e4
DevTekVE <devtekve@gmail.com>
2026-04-03 16:49:48 +0200

    linter v2 lol

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -416,7 +416,7 @@ class CAR(Platforms):
   )
   HYUNDAI_IONIQ_9 = HyundaiCanFDPlatformConfig(
     [
-      HyundaiCarDocs("Hyundai Ioniq 9 (with HDA II & LFA2) 2025-26", "Highway Driving Assist II & Lane Follow Assist 2", 
+      HyundaiCarDocs("Hyundai Ioniq 9 (with HDA II & LFA2) 2025-26", "Highway Driving Assist II & Lane Follow Assist 2",
                      car_parts=CarParts.common([CarHarness.hyundai_m]))
     ],
     CarSpecs(mass=2700, wheelbase=3.13, steerRatio=16.02),
