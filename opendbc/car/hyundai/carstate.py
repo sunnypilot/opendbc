efd7b146090faa6e0341da4c7d4126dd2a91e87e
royjr <royjr96@gmail.com>
2026-04-08 00:55:26 -0400

    Update carstate.py

--- opendbc/car/hyundai/carstate.py
+++ opendbc/car/hyundai/carstate.py
@@ -262,7 +262,7 @@ class CarState(CarStateBase, EsccCarStateBase, MadsCarState, CarStateExt):
     alt = ""
     if self.CP.flags & HyundaiFlags.CCNC:
       alt = "_ALT"
-      if not self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING:
+      if not self.CP.flags & HyundaiFlags.CANFD_LKA_STEER_MSG:
         self.msg_161, self.msg_162, self.msg_1b5 = map(copy.copy, (cp_cam.vl["CCNC_0x161"], cp_cam.vl["CCNC_0x162"], cp_cam.vl["FR_CMR_03_50ms"]))
         self.cruise_info = copy.copy((cp_cam if self.CP.flags & HyundaiFlags.CANFD_CAMERA_SCC else cp).vl["SCC_CONTROL"])
     ret.leftBlinker, ret.rightBlinker = self.update_blinker_from_lamp(50, cp.vl["BLINKERS"][f"LEFT_LAMP{alt}"],
