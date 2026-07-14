54a87911545b1788319c8a8113a11011e4d3a825
royjr <royjr96@gmail.com>
2026-07-14 00:34:23 -0400

    Update values.py

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -472,7 +472,7 @@ class CAR(Platforms):
   )
   KIA_K4_2025 = HyundaiCanFDPlatformConfig(
     [
-      HyundaiCarDocs("Kia K4 (without HDA II) 2025", car_parts=CarParts.common([CarHarness.hyundai_a])),
+      HyundaiCarDocs("Kia K4 (without HDA II) 2025-26", car_parts=CarParts.common([CarHarness.hyundai_a])),
       HyundaiCarDocs("Kia K4 (with HDA II) 2025", car_parts=CarParts.common([CarHarness.hyundai_r])),
     ],
     CarSpecs(mass=2987 * CV.LB_TO_KG, wheelbase=2.72, steerRatio=13.4),
