de99cf84d20e8d582516a980b78558585393ba54
royjr <royjr96@gmail.com>
2026-07-15 21:31:43 -0400

    RADAR_3A5_3C4

--- opendbc/dbc/generator/hyundai/hyundai_radar_3a5_3c4.py
+++ opendbc/dbc/generator/hyundai/hyundai_radar_3a5_3c4.py
@@ -47,32 +47,23 @@ BU_: XXX
 BO_ {a} RADAR_TRACK_{a:x}: 24 RADAR
  SG_ CHECKSUM : 0|16@1+ (1,0) [0|65535] "" XXX
  SG_ COUNTER : 16|8@1+ (1,0) [0|255] "" XXX
- SG_ NEW_SIGNAL_1 : 25|2@0+ (1,0) [0|3] "" XXX
- SG_ NEW_SIGNAL_3 : 28|2@0+ (1,0) [0|3] "" XXX
- SG_ COUNTER_3 : 31|2@0+ (1,0) [0|3] "" XXX
- SG_ NEW_SIGNAL_2 : 38|7@0- (1,0) [0|127] "" XXX
+ SG_ STATE_ALT : 25|2@0+ (1,0) [0|3] "" XXX
+ SG_ MOTION_STATE : 28|2@0+ (1,0) [0|3] "" XXX
+ SG_ TRACK_COUNTER : 31|2@0+ (1,0) [0|3] "" XXX
+ SG_ NEW_SIGNAL_2 : 38|7@0- (1,0) [-64|63] "" XXX
  SG_ AGE : 47|8@0+ (1,0) [0|255] "" XXX
- SG_ NEW_SIGNAL_6 : 51|4@0+ (1,0) [0|15] "" XXX
+ SG_ COAST_AGE : 51|4@0+ (1,0) [0|15] "" XXX
  SG_ STATE : 54|3@0+ (1,0) [0|7] "" XXX
- SG_ NEW_SIGNAL_8 : 62|7@0- (1,0) [0|127] "" XXX
- SG_ LONG_DIST : 63|12@1+ (0.05,0) [0|8191] "m" XXX
- SG_ LAT_DIST : 76|12@1- (0.05,0) [0|127] "" XXX
- SG_ REL_SPEED : 88|14@1- (0.01,0) [0|16383] "" XXX
+ SG_ NEW_SIGNAL_8 : 62|7@0- (1,0) [-64|63] "" XXX
+ SG_ LONG_DIST : 63|12@1+ (0.05,0) [0|204.75] "m" XXX
+ SG_ LAT_DIST : 76|12@1- (0.05,0) [-102.4|102.35] "m" XXX
+ SG_ REL_SPEED : 88|14@1- (0.01,0) [-81.92|81.91] "m/s" XXX
  SG_ NEW_SIGNAL_4 : 103|2@0+ (1,0) [0|3] "" XXX
- SG_ LAT_DIST_ACCEL : 104|13@1- (1,0) [0|8191] "" XXX
- SG_ REL_ACCEL : 118|10@1- (0.02,0) [0|1023] "" XXX
- SG_ NEW_SIGNAL_18 : 129|2@0+ (1,0) [0|3] "" XXX
- SG_ NEW_SIGNAL_5 : 133|4@0+ (1,0) [0|15] "" XXX
- SG_ NEW_SIGNAL_10 : 138|5@1+ (1,0) [0|31] "" XXX
- SG_ NEW_SIGNAL_11 : 149|6@0+ (1,0) [0|63] "" XXX
- SG_ NEW_SIGNAL_7 : 152|10@1+ (1,0) [0|1023] "" XXX
- SG_ NEW_SIGNAL_9 : 162|9@1- (1,0) [0|511] "" XXX
- SG_ NEW_SIGNAL_13 : 175|4@0+ (1,0) [0|15] "" XXX
- SG_ NEW_SIGNAL_12 : 179|4@0+ (1,0) [0|15] "" XXX
- SG_ NEW_SIGNAL_14 : 181|2@0+ (1,0) [0|3] "" XXX
- SG_ NEW_SIGNAL_15 : 183|2@0+ (1,0) [0|3] "" XXX
- SG_ NEW_SIGNAL_16 : 185|2@0+ (1,0) [0|3] "" XXX
- SG_ NEW_SIGNAL_17 : 187|2@0+ (1,0) [0|3] "" XXX
+ SG_ REL_LAT_SPEED : 104|13@1- (0.01,0) [-40.96|40.95] "m/s" XXX
+ SG_ REL_ACCEL : 118|10@1- (0.02,0) [-10.24|10.22] "m/s^2" XXX
+ SG_ NEW_SIGNAL_5 : 134|5@0+ (1,0) [0|31] "" XXX
+
+VAL_ {a} MOTION_STATE 0 "UNKNOWN" 1 "STATIONARY" 2 "MOVING" ;
     """)
 
   return {"hyundai_radar_3a5_3c4.dbc": "".join(parts)}
