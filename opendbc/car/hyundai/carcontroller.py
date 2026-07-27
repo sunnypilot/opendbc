e6af8969b6d582266b76cb6cc3000ec04e6177d1
Jason Wen <haibin.wen3@gmail.com>
2026-07-27 18:18:02 -0400

    good luck devtekve

--- opendbc/car/hyundai/carcontroller.py
+++ opendbc/car/hyundai/carcontroller.py
@@ -50,7 +50,7 @@ def compute_torque_reduction_gain(steering_torque, v_ego, lat_active, last_gain)
   if lat_active:
     ceiling = np.interp(v_ego, [0.5, 1.5], [1.0, 0.85])
     shelf = np.interp(v_ego, [2, 11], [0.45, 0.6])
-    floor = np.interp(v_ego, [2, 22], [0.1, 0.3])
+    floor = np.interp(v_ego, [2, 22], [0.5, 0.3])
     bp1 = np.interp(v_ego, [2, 11], [75, 125])
     bp2 = np.interp(v_ego, [2, 11], [125, 150])
     bp3 = np.interp(v_ego, [2, 11], [175, 275])
