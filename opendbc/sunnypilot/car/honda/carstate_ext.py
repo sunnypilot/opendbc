f13bb3e5bbb63098207ed4853c86d0a677d4adda
MVL <manuel@alumni.virginia.edu>
2026-08-10 18:11:05 -0400

    Allow brakePressed in B mode for Honda carstate

--- opendbc/sunnypilot/car/honda/carstate_ext.py
+++ opendbc/sunnypilot/car/honda/carstate_ext.py
@@ -32,3 +32,6 @@ class CarStateExt:
       # Same threshold as panda, equivalent to 1e-5 with previous DBC scaling
       gas = (cp.vl["GAS_SENSOR"]["INTERCEPTOR_GAS"] + cp.vl["GAS_SENSOR"]["INTERCEPTOR_GAS2"]) // 2
       ret.gasPressed = gas > 492
+
+    if ret.gearShifter == GearShifter.brake
+      ret.brakePressed = True # allows MADS in B (regen braking) mode
