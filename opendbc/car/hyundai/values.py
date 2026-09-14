aa0e25f864e8e722c1fd061e7e0daba5dd6c3f5d
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-09-14 14:35:23 -0400

    fingerprint: 2026 Kia EV6 w/ HDA II

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -636,7 +636,8 @@ class CAR(Platforms):
   )
   KIA_EV6_2025 = HyundaiCanFDPlatformConfig(
     [
-      HyundaiCarDocs("Kia EV6 (with HDA I) 2025", "Highway Driving Assist I", car_parts=CarParts.common([CarHarness.hyundai_p]))
+      HyundaiCarDocs("Kia EV6 (with HDA I) 2025", "Highway Driving Assist I", car_parts=CarParts.common([CarHarness.hyundai_p])),
+      HyundaiCarDocs("Kia EV6 (with HDA II) 2025-26", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_p]))
     ],
     CarSpecs(mass=2055, wheelbase=2.9, steerRatio=16, tireStiffnessFactor=0.65),
     flags=HyundaiFlags.EV | HyundaiFlags.CANFD_ANGLE_STEERING,
