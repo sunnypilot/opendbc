aad0089ca40aa53b4a3611dc479e03b7bea6650d
Kumar <36933347+rav4kumar@users.noreply.github.com>
2026-09-17 09:35:44 -0700

    Update interfaces.py

--- opendbc/car/interfaces.py
+++ opendbc/car/interfaces.py
@@ -245,7 +245,7 @@ class CarInterfaceBase(ABC, CarInterfaceBaseSP):
     ret.minEnableSpeed = -1. # enable is done by stock ACC, so ignore this
     ret.steerRatioRear = 0.  # no rear steering, at least on the listed cars aboveA
     ret.openpilotLongitudinalControl = False
-    ret.stopAccel = -2.0
+    ret.stopAccel = -0.04
     ret.longitudinalTuning.kiBP = [0.]
     ret.longitudinalTuning.kiV = [0.]
     # TODO estimate car specific lag, use .15s for now
